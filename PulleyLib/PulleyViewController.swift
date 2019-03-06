cac0d3f3d1a20f0963767ff96c6b584344bd126c
Ash Furrow <ash@ashfurrow.com>
2019-03-06 14:20:06 -0500

    Exposes initialDrawerPosition to ObjC interop.

--- PulleyLib/PulleyViewController.swift
+++ PulleyLib/PulleyViewController.swift
@@ -427,7 +427,7 @@ open class PulleyViewController: UIViewController, PulleyDrawerViewControllerDel
     }
     
     /// The starting position for the drawer when it first loads
-    public var initialDrawerPosition: PulleyPosition = .partiallyRevealed
+    @objc public var initialDrawerPosition: PulleyPosition = .partiallyRevealed
     
     /// The display mode for Pulley. Default is 'drawer', which preserves the previous behavior of Pulley. If you want it to adapt automatically, choose 'automatic'. The current display mode is available by using the 'currentDisplayMode' property.
     public var displayMode: PulleyDisplayMode = .drawer {
